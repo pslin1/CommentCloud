8a6c0336b72efa15f7f7e0600196ca292f47a7ec
Pierre Lin <pslin@ualberta.ca>
2021-02-07 13:14:11 -0800

    Pulls comments from user in reddit instance

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 
 import praw
 
+#Read only mode
 reddit = praw.Reddit('CommentCloud')
 
 print(reddit.read_only)
+
+user = reddit.redditor("reddit_username_here")
+for comment in user.comments.new(limit=None):
+    print(comment.body)
